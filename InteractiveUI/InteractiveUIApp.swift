412ace9502112dd0c3339b7c9890c44cd1db669b
Maya Stein <mayarosestein@gmail.com>
2025-04-17 08:42:20 -0400

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Maya Stein on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InteractiveUITests/InteractiveUITests.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUITests.swift
+//  InteractiveUITests
+//
+//  Created by Maya Stein on 4/17/25.
+//
+
+import Testing
+@testable import InteractiveUI
+
+struct InteractiveUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
