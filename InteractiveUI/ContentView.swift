ea54345caaba18607fa740e6c71cc348f8904a02
Maya Stein <mayarosestein@gmail.com>
2025-04-17 08:52:27 -0400

    Added the structure of the app, complete except the UI features

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -14,8 +14,19 @@ struct ContentView: View {
                 .font(.title)
                 .fontWeight(.black)
                 .foregroundColor(Color(hue: 0.611, saturation: 0.44, brightness: 0.784))
-                
+            TextField("Type your name here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding(.all)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.blue, width:1)
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.blue)
         }
+        .padding(.all)
     }
 }
 
