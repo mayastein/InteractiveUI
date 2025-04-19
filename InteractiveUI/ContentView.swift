e43723be8b7191a4c8b54c3ade856235bacf5c3d
Maya Stein <mayarosestein@gmail.com>
2025-04-19 12:59:30 -0400

    Completed MVP, adding date selection tool and alerts on submit

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,26 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name=""
+    @State private var fName=""
+    @State private var lName=""
     @State private var title="What is your name?"
+    @State private var presentA=false
+    @State private var bTitle="What is your birthday?"
+    @State private var date=Date()
     var body: some View {
         VStack {
             Text(title)
                 .font(.title)
                 .fontWeight(.black)
                 .foregroundColor(Color(hue: 0.611, saturation: 0.44, brightness: 0.784))
-            TextField("Type your name here", text: $name)
+            TextField("Type your first name here", text: $fName)
                 .padding(.all)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.blue, width:1)
+                .foregroundColor(Color(red: 0.64, green: 0.513, blue: 0.711))
+            TextField("Type your last name here", text: $lName)
+                .padding(.all)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.blue, width:1)
+                .foregroundColor(Color(red: 0.64, green: 0.513, blue: 0.711))
             Button("Submit Name") {
-                title="Hello There, \(name)!"
+                presentA=true
+                title="Hello There, \(fName) \(lName)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.blue)
+            Text(bTitle).font(.title)
+                .fontWeight(.black)
+                .foregroundColor(Color(hue: 0.611, saturation: 0.44, brightness: 0.784))
+                .padding(.top)
+            DatePicker(
+                "Birthday",
+                selection: $date,
+                displayedComponents: [.date])
+            .foregroundColor(Color(hue: 0.774, saturation: 0.31, brightness: 0.686))
+            .font(.title2)
+            Button("Submit Date") {
+                presentA=true
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.blue)
         }
+        .alert("Got it!", isPresented: $presentA, actions:{
+            
+        })
         .padding(.all)
     }
 }
